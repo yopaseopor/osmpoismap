9cc749735d4eaca281aff3d4eb175172687385df
yopaseopor <yopaseopor@gmail.com>
2022-09-22 19:05:53 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -224,6 +224,42 @@ var config = {
 				});
 				return style;
 			}
+  },
+		{
+			group: 'Test',
+			title: 'Fire hydrant',
+			query: '(nwr["highway"="track"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/fire_hydrant.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(117,63,79,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(117,63,79,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'icones/hydrant_sign.svg',
+							scale:0.07
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -10,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
 		},
 		{
 			group: 'Test',
