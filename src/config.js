ed692daba7b572301c3fbbaebefad01892602237
yopaseopor <yopaseopor@gmail.com>
2022-09-22 19:43:15 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -245,7 +245,7 @@ style: function (feature) {
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'icones/maxspeed_empty.svg',
-							scale:0.07
+							scale:0.03
 						}),
 							text: new ol.style.Text({
 								text: name,
