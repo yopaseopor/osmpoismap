abca31c67037e6596276b4687a273805cbb531c4
yopaseopor <yopaseopor@gmail.com>
2022-11-22 21:24:51 +0100

    Citroen

--- src/config.js
+++ src/config.js
@@ -575,6 +575,42 @@ style: function (feature) {
 				},
 		{
 			group: 'Marcas & Movilidad',
+			title: 'Citroën',
+			query: '(nwr["brand:wikidata"="Q152057"]({{bbox}});node(w););out meta;',
+			iconSrc: 'https://raw.githubusercontent.com/yopaseopor/osmpoismap/master/src/img/logos/citroen.png',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'https://raw.githubusercontent.com/yopaseopor/osmpoismap/master/src/img/logos/citroen.png',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		{
+			group: 'Marcas & Movilidad',
 			title: 'BP',
 			query: '(nwr["brand:wikidata"="Q152057"]({{bbox}});node(w););out meta;',
 			iconSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Bp_textlogo.svg/220px-Bp_textlogo.svg.png',
