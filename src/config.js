5c9048f52899c1784f882775a519f98c4baa14a1
yopaseopor <yopaseopor@gmail.com>
2022-12-13 00:15:05 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -14458,7 +14458,7 @@ style: function (feature) {
 },
 		{	
 			group: 'BCN (CC 4.0 Dades Ajuntament BCN)',
-			title: 'Restauració',
+			title: 'Restauració2',
 			geojson: 'https://raw.githubusercontent.com/yopaseopor/osmpoismap/master/src/test_bcn.geojson',
 			iconSrc: 'https://raw.githubusercontent.com/yopaseopor/osmpoismap/master/src/img/icones/eat_drink.svg',
 			iconStyle: 'background-color:rgba(255,255,255,0.4)',
