d4dbef69220abda3a37e483c3da41791a9cf4bec
yopaseopor <yopaseopor@gmail.com>
2023-03-06 21:42:36 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -14923,47 +14923,7 @@ style: function (feature) {
 			}
 		}
 	],
-	
-	// Set the search control 
-var searchNominatim = new ol.control.SearchNominatim (
-    {   target: $(".options").get(0),
-      polygon: $("#polygon").prop("checked"),
-        reverse: true,
-        position: true, // Search, with priority to geo position
-        title: "Busca",
-        reverseTitle: "Clique no mapa...",
-        placeholder: "Busca..."
-    });
-map.addControl (searchNominatim);
 
-// Select feature when click on the reference index
-searchNominatim.on('select', function(e)
-{   
-//  sLayer.getSource().clear();
-    // Check if we get a geojson to describe the search
-    if (e.search.geojson) {
-        var format = new ol.format.GeoJSON();
-        var f = format.readFeature(e.search.geojson, { dataProjection: "EPSG:4326", featureProjection: map.getView().getProjection() });
-    //  sLayer.getSource().addFeature(f);
-        var view = map.getView();
-        var resolution = view.getResolutionForExtent(f.getGeometry().getExtent(), map.getSize());
-        var zoom = view.getZoomForResolution(resolution);
-        var center = ol.extent.getCenter(f.getGeometry().getExtent());
-        // redraw before zoom
-        setTimeout(function(){
-                view.animate({
-                center: center,
-                zoom: Math.min (zoom, 16)
-            });
-        }, 100);
-    }
-    else {
-        map.getView().animate({
-            center:e.coordinate,
-            zoom: Math.max (map.getView().getZoom(),16)
-        });
-    }
-});
 
 	//Es crida sempre que es fa click sobre el mapa
 	onClickEvent: function(evt, view, coordinateLL) {
