706047d4dc6e127aa12ec234de88a88570207de9
yopaseopor <yopaseopor@gmail.com>
2023-03-07 00:13:32 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -331,6 +331,54 @@ $(function () {
 	}));
 
 	// Info Control
+	var searchNominatim = new ol.control.SearchNominatim (
+{   
+    //  polygon: $("#polygon").prop("checked"),
+        reverse: true,
+        polygon: false,
+        position: true, // Search, with priority to geo position
+        title: "Busca",
+        reverseTitle: "Clique no mapa...",
+        placeholder: "Busca...",
+//      target: $("#address > div").get(0)
+});
+var requestData = searchNominatim.requestData.bind(searchNominatim);
+searchNominatim.requestData = function (s) {
+    var data = requestData(s);
+    data.countrycodes = 'br';
+    data.citt
+    return data;
+};
+map.addControl (searchNominatim);
+
+// Select feature when click on the reference index
+searchNominatim.on('select', function(e)
+{   
+//  sLayer.getSource().clear();
+    // Check if we get a geojson to describe the search
+    if (e.search.geojson) {
+        var format = new ol.format.GeoJSON();
+        var f = format.readFeature(e.search.geojson, { dataProjection: "EPSG:4326", featureProjection: map.getView().getProjection() });
+    //  sLayer.getSource().addFeature(f);
+        var view = map.getView();
+        var resolution = view.getResolutionForExtent(f.getGeometry().getExtent(), map.getSize());
+        var zoom = view.getZoomForResolution(resolution);
+        var center = ol.extent.getCenter(f.getGeometry().getExtent());
+        // redraw before zoom
+        setTimeout(function(){
+                view.animate({
+                center: center,
+                zoom: Math.min (zoom, 16)
+            });
+        }, 100);
+    }
+    else {
+        map.getView().animate({
+            center:e.coordinate,
+            zoom: Math.max (map.getView().getZoom(),16)
+        });
+    }
+}
 	var infoControlBuild = function () {
 		var container = $('<div>').addClass('ol-control ol-unselectable osmcat-infobutton').html($('<button type="button"><i class="fa fa-info-circle"></i></button>').on('click', function () {
 			window.location.href = 'javascript:searchNominatim.select()';
@@ -351,6 +399,8 @@ $(function () {
 	map.addControl(new ol.control.Control({
 		element: infoControlBuild2()
 	}));
+	
+	
 
 	// Copy permalink button
 	var permalinkControlBuild = function () {
@@ -563,54 +613,7 @@ $(function () {
 });
 
 // Set the search control 
-var searchNominatim = new ol.control.SearchNominatim (
-{   
-    //  polygon: $("#polygon").prop("checked"),
-        reverse: true,
-        polygon: false,
-        position: true, // Search, with priority to geo position
-        title: "Busca",
-        reverseTitle: "Clique no mapa...",
-        placeholder: "Busca...",
-//      target: $("#address > div").get(0)
-});
-var requestData = searchNominatim.requestData.bind(searchNominatim);
-searchNominatim.requestData = function (s) {
-    var data = requestData(s);
-    data.countrycodes = 'br';
-    data.citt
-    return data;
-};
-map.addControl (searchNominatim);
 
-// Select feature when click on the reference index
-searchNominatim.on('select', function(e)
-{   
-//  sLayer.getSource().clear();
-    // Check if we get a geojson to describe the search
-    if (e.search.geojson) {
-        var format = new ol.format.GeoJSON();
-        var f = format.readFeature(e.search.geojson, { dataProjection: "EPSG:4326", featureProjection: map.getView().getProjection() });
-    //  sLayer.getSource().addFeature(f);
-        var view = map.getView();
-        var resolution = view.getResolutionForExtent(f.getGeometry().getExtent(), map.getSize());
-        var zoom = view.getZoomForResolution(resolution);
-        var center = ol.extent.getCenter(f.getGeometry().getExtent());
-        // redraw before zoom
-        setTimeout(function(){
-                view.animate({
-                center: center,
-                zoom: Math.min (zoom, 16)
-            });
-        }, 100);
-    }
-    else {
-        map.getView().animate({
-            center:e.coordinate,
-            zoom: Math.max (map.getView().getZoom(),16)
-        });
-    }
-});
 
 function linearColorInterpolation(colorFrom, colorTo, weight) {
 	var p = weight < 0 ? 0 : (weight > 1 ? 1 : weight),
