72a8415fc97ca6992f8fc9ec40527a24b1aad69d
yopaseopor <yopaseopor@gmail.com>
2023-03-06 23:51:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -333,7 +333,7 @@ $(function () {
 	// Info Control
 	var infoControlBuild = function () {
 		var container = $('<div>').addClass('ol-control ol-unselectable osmcat-infobutton').html($('<button type="button"><i class="fa fa-info-circle"></i></button>').on('click', function () {
-			window.location.href = 'https://github.com/yopaseopor/osmffmap';
+			window.location.href = 'javascript:searchNominatim.select()';
 		}));
 		return container[0];
 	};
@@ -344,7 +344,7 @@ $(function () {
 		// Info Control2
 	var infoControlBuild2 = function () {
 		var container = $('<div>').addClass('ol-control ol-unselectable osmcat-infobutton2').html($('<button type="button"><i class="fa fa-search-plus"></i></button>').on('click', function () {
-			window.location.href = 'javascript:searchNominatim.select()';
+			window.location.href = 'https://github.com/yopaseopor/osmpoismap';
 		}));
 		return container[0];
 	};
