22c63fae8aae2b1c47946a771b06b6c6145988d6
yopaseopor <yopaseopor@gmail.com>
2023-03-06 19:09:24 +0100

    test

--- src/index.js
+++ src/index.js
@@ -235,45 +235,7 @@ $(function () {
 					});
 				});
 				
-				//	
-			{
-  const olview = new ol.View({
-    center: [0, 0],
-    zoom: 3,
-    minZoom: 2,
-    maxZoom: 20,
-  });
-
-  const baseLayer = new ol.layer.Tile({
-    source: new ol.source.OSM(),
-  });
-  const map = new ol.Map({
-    target: doc.querySelector('#map'),
-    view: olview,
-    layers: [baseLayer],
-  });
-  const popup = new ol.Overlay.Popup();
-
-  // Instantiate with some options and add the Control
-  const geocoder = new Geocoder('nominatim', {
-    provider: 'osm',
-    targetType: 'text-input',
-    lang: 'en',
-    placeholder: 'Search for ...',
-    limit: 5,
-    keepOpen: false,
-  });
-
-  map.addControl(geocoder);
-  map.addOverlay(popup);
-
-  // Listen when an address is chosen
-  geocoder.on('addresschosen', (evt) => {
-    window.setTimeout(() => {
-      popup.show(evt.coordinate, evt.address.formatted);
-    }, 3000);
-  });
-  //
+				
 
 				overlayDiv.append(overlayDivContent);
 				overlayDiv.show();
@@ -341,6 +303,46 @@ $(function () {
 	}));
 	map.addControl(new ol.control.ScaleLine({units: config.initialConfig.units}));
 	map.addControl(new ol.control.ZoomSlider());
+	
+	//	
+			{
+  const olview = new ol.View({
+    center: [0, 0],
+    zoom: 3,
+    minZoom: 2,
+    maxZoom: 20,
+  });
+
+  const baseLayer = new ol.layer.Tile({
+    source: new ol.source.OSM(),
+  });
+  const map = new ol.Map({
+    target: doc.querySelector('#map'),
+    view: olview,
+    layers: [baseLayer],
+  });
+  const popup = new ol.Overlay.Popup();
+
+  // Instantiate with some options and add the Control
+  const geocoder = new Geocoder('nominatim', {
+    provider: 'osm',
+    targetType: 'text-input',
+    lang: 'en',
+    placeholder: 'Search for ...',
+    limit: 5,
+    keepOpen: false,
+  });
+
+  map.addControl(geocoder);
+  map.addOverlay(popup);
+
+  // Listen when an address is chosen
+  geocoder.on('addresschosen', (evt) => {
+    window.setTimeout(() => {
+      popup.show(evt.coordinate, evt.address.formatted);
+    }, 3000);
+  });
+  //
 
 	// Geolocation Control
 	// In some browsers, this feature is available only in secure contexts (HTTPS)
