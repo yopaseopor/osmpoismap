f973d96cbbe45e290fcb8e8c573feddcfee4a6a5
yopaseopor <yopaseopor@gmail.com>
2023-05-27 05:11:20 +0200

    100 montaditos

--- src/config.js
+++ src/config.js
@@ -979,6 +979,42 @@ style: function (feature) {
 				},
 		{
 			group: 'Marcas & Restauración',
+			title: '100 Montaditos',
+			query: '(nwr["brand:wikidata"="Q8355805"]({{bbox}});node(w););out meta;',
+			iconSrc: 'src/img/logos/100montaditos.jpg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'src/img/logos/100montaditos.jpg',
+							scale:0.10
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		{
+			group: 'Marcas & Restauración',
 			title: 'Domino`s Pizza',
 			query: '(nwr["brand:wikidata"="Q839466"]({{bbox}});node(w););out meta;',
 			iconSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Dominos_pizza_logo.svg/220px-Dominos_pizza_logo.svg.png',
