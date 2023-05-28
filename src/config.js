4d2aef8914b8661b283e2ce0a55feac907ee7c46
yopaseopor <yopaseopor@gmail.com>
2023-05-29 00:07:28 +0200

    La Sureña

--- src/config.js
+++ src/config.js
@@ -1195,6 +1195,42 @@ style: function (feature) {
 				},
 		{
 			group: 'Marcas & Restauración',
+			title: 'La Sureña',
+			query: '(nwr["brand:wikidata"="Q5763573"]({{bbox}});node(w););out meta;',
+			iconSrc: 'src/img/logos/lasurena.jpg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'src/img/logos/lasurena.jpg',
+							scale:0.10
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		{
+			group: 'Marcas & Restauración',
 			title: 'La Tagliatella',
 			query: '(nwr["brand:wikidata"="Q113426257"]({{bbox}});node(w););out meta;',
 			iconSrc: 'https://www.amrest-franquicias.com/sites/default/files/logo_tagliatella_clean2x.png',
