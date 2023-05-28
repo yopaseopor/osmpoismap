2c6a3ca8dd2348151a032d61128a5a089cbed325
yopaseopor <yopaseopor@gmail.com>
2023-05-29 00:15:38 +0200

    Vips

--- src/config.js
+++ src/config.js
@@ -1806,6 +1806,42 @@ style: function (feature) {
 			}
 				},
 		{
+			group: 'Marcas & Restauración',
+			title: 'Vips',
+			query: '(nwr["brand:wikidata"="Q66087862"]({{bbox}});node(w););out meta;',
+			iconSrc: 'src/img/logos/vips.png',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'src/img/logos/vips.png',
+							scale:0.10
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		{
 			group: 'Marcas & Alimentación',
 			title: 'Alcampo',
 			query: '(nwr["brand:wikidata"="Q2832081"]({{bbox}});node(w););out meta;',
