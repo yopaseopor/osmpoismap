a33596e8cfe07694f8e3d7ee08287267f2822bd1
yopaseopor <yopaseopor@gmail.com>
2023-06-08 01:19:01 +0200

    Game

--- src/config.js
+++ src/config.js
@@ -4570,7 +4570,7 @@ style: function (feature) {
 				});
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
-							src: 'src/img/logos/aasepeyo.jpg',
+							src: 'src/img/logos/asepeyo.jpg',
 							scale:0.10
 						}),
 							text: new ol.style.Text({
@@ -4756,6 +4756,43 @@ style: function (feature) {
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Fliyingtiger.svg/220px-Fliyingtiger.svg.png',
+							scale:0.10
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		
+		{
+			group: 'Marcas & Salud',
+			title: 'Game',
+			query: '(nwr["brand:wikidata"="Q5519813"]({{bbox}});node(w););out meta;',
+			iconSrc: 'src/img/logos/game.png',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'src/img/logos/game.png',
 							scale:0.10
 						}),
 							text: new ol.style.Text({
