986066c153d831b81652554cbed71b558285fc89
yopaseopor <yopaseopor@gmail.com>
2023-06-12 15:48:08 +0200

    huawei uniqlo

--- src/config.js
+++ src/config.js
@@ -5802,7 +5802,44 @@ style: function (feature) {
 		
 		{
 			group: 'Marcas & Moda',
-			title: 'Unnited Colors of Benetton',
+			title: 'Uniqlo',
+			query: '(nwr["brand:wikidata"="Q26070"]({{bbox}});node(w););out meta;',
+			iconSrc: 'src/img/logos/uniqlo.jpg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'src/img/logos/uniqlo.jpg',
+							scale:0.10
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		
+		{
+			group: 'Marcas & Moda',
+			title: 'United Colors of Benetton',
 			query: '(nwr["brand:wikidata"="Q817139"]({{bbox}});node(w););out meta;',
 			iconSrc: 'src/img/logos/united_colors.jpg',
 			iconStyle: 'background-color:rgba(255,255,255,0.4)',
@@ -7925,6 +7962,43 @@ style: function (feature) {
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/TEDi-Logo.svg/220px-TEDi-Logo.svg.png',
+							scale:0.10
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		
+		{
+			group: 'Marcas & Comunicaciones',
+			title: 'Huawei',
+			query: '(nwr["brand:wikidata"="Q160120"]({{bbox}});node(w););out meta;',
+			iconSrc: 'src/img/logos/huawei.png',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'src/img/logos/huawei.png',
 							scale:0.10
 						}),
 							text: new ol.style.Text({
