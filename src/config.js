51c02a80f74835a1e6718e3cb69e718d5259e84b
yopaseopor <yopaseopor@gmail.com>
2023-06-16 01:11:51 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -179,27 +179,27 @@ var config = {
 		
 				
 		
-		{	
-			group: 'Test',
-			title: 'Burger King',
-			query: '(nwr["brand:wikidata"="Q177054"]({{bbox}});node(w););out meta;',
-			iconSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Burger_King_2020.svg/220px-Burger_King_2020.svg.png',
+{
+			group: 'Alimentación',
+			title: 'Supermercados',
+			query: '(nwr["shop"="supermarket"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/maxspeed_empty.svg',
 			iconStyle: 'background-color:rgba(255,255,255,0.4)',
 style: function (feature) {
 				var key_regex = /^name$/
 				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
 				var name = feature.get(name_key) || '';
 				var fill = new ol.style.Fill({
-					color: 'rgba(255,0,0,0.4)'
+					color: 'rgba(117,63,79,0.4)'
 				});
 				var stroke = new ol.style.Stroke({
-					color: 'rgba(255,0,0,1)',
+					color: 'rgba(117,63,79,1)',
 					width: 1
 				});
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
-							src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Burger_King_2020.svg/220px-Burger_King_2020.svg.png',
-							scale:0.10
+							src: imgSrc + 'icones/maxspeed_empty.svg',
+							scale:0.03
 						}),
 							text: new ol.style.Text({
 								text: name,
@@ -217,7 +217,7 @@ style: function (feature) {
 
 /*@@ inicio-fin de copia */			},
 /*   abrir */							{
-/*@@ nombre del grupo al que pertenecen */	group: 'Test',
+/*@@ nombre del grupo al que pertenecen */	group: 'Economía',
 /*@@ título de la opción */					title: 'Banco Sabadell',
 /*@@ consulta overpass */					query: '(nwr["brand:wikidata"="Q762330"]({{bbox}});node(w););out meta;',
 /*@@ ruta del icono (URL o relativa) */		iconSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSabadell_Logo.svg/220px-BSabadell_Logo.svg.png',
@@ -3679,43 +3679,7 @@ style: function (feature) {
 				});
 				return style;
 			}
-				},
-			
-		{
-			group: 'Economía',
-			title: 'Banco Sabadell',
-			query: '(nwr["brand:wikidata"="Q762330"]({{bbox}});node(w););out meta;',
-			iconSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSabadell_Logo.svg/220px-BSabadell_Logo.svg.png',
-			iconStyle: 'background-color:rgba(255,255,255,0.4)',
-style: function (feature) {
-				var key_regex = /^name$/
-				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
-				var name = feature.get(name_key) || '';
-				var fill = new ol.style.Fill({
-					color: 'rgba(255,0,0,0.4)'
-				});
-				var stroke = new ol.style.Stroke({
-					color: 'rgba(255,0,0,1)',
-					width: 1
-				});
-				var style = new ol.style.Style({
-					image: new ol.style.Icon({
-							src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/BSabadell_Logo.svg/220px-BSabadell_Logo.svg.png',
-							scale:0.30
-						}),
-							text: new ol.style.Text({
-								text: name,
-								offsetX : 7,
-								offsetY : -12,
-								fill: new ol.style.Fill({
-                            color: 'rgba(0,0,0,1)'
-                        }),
-						}),
-					fill: fill,
-					stroke: stroke
-				});
-				return style;
-			}
+
 				},
 		{
 			group: 'Economía',
