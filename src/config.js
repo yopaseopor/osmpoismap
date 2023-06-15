db7d3ba0bb5c18eba54d901c2d58b232126249c4
yopaseopor <yopaseopor@gmail.com>
2023-06-16 01:16:04 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -366,41 +366,6 @@ style: function (feature) {
 
 				},
 		
-		{
-			group: 'Alimentación',
-			title: 'Supermercados',
-			query: '(nwr["shop"="supermarket"]({{bbox}});node(w););out meta;',
-			iconSrc: imgSrc + 'icones/maxspeed_empty.svg',
-			iconStyle: 'background-color:rgba(255,255,255,0.4)',
-style: function (feature) {
-				var key_regex = /^name$/
-				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
-				var name = feature.get(name_key) || '';
-				var fill = new ol.style.Fill({
-					color: 'rgba(117,63,79,0.4)'
-				});
-				var stroke = new ol.style.Stroke({
-					color: 'rgba(117,63,79,1)',
-					width: 1
-				});
-				var style = new ol.style.Style({
-					image: new ol.style.Icon({
-							src: imgSrc + 'icones/maxspeed_empty.svg',
-							scale:0.03
-						}),
-							text: new ol.style.Text({
-								text: name,
-								offsetX : 7,
-								offsetY : -12,
-								fill: new ol.style.Fill({
-                            color: 'rgba(0,0,0,1)'
-                        }),
-						}),
-					fill: fill,
-					stroke: stroke
-				});
-				return style;
-			}
 		},
 		{
 			group: 'Alimentación',
