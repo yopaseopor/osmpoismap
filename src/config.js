856aeb06c99ff6b9acdaf9f37ff7ef8fefbf40d3
yopaseopor <yopaseopor@gmail.com>
2023-06-19 22:32:05 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -544,6 +544,43 @@ style: function (feature) {
 				});
 				return style;
 			}
+		},
+
+		{
+			group: 'Alimentación',
+			title: 'A granel',
+			query: '(nwr["bulk_purchase"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/maxspeed_empty.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'icones/maxspeed_empty.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
 				},
 		{
 			group: 'Alimentación',
