34c77470bc74a650890559ee07ddc0f5f9ca1a2b
yopaseopor <yopaseopor@gmail.com>
2023-07-05 12:32:08 +0200

    alehop

--- src/config.js
+++ src/config.js
@@ -4768,6 +4768,42 @@ style: function (feature) {
 				},
 		{
 			group: 'Comercio',
+			title: 'ALE-HOP',
+			query: '(nwr["brand:wikidata"="Q98456840"]({{bbox}});node(w););out meta;',
+			iconSrc: 'src/img/logos/alehop.jpg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: 'src/img/logos/alehop.jpg',
+							scale:0.10
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		{
+			group: 'Comercio',
 			title: 'Flying Tiger',
 			query: '(nwr["brand:wikidata"="Q2786319"]({{bbox}});node(w););out meta;',
 			iconSrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8b/Fliyingtiger.svg/220px-Fliyingtiger.svg.png',
@@ -14562,7 +14598,7 @@ style: function (feature) {
 				},
 		{
 			group: 'Ocio',
-			title: 'Souvenirs',
+			title: 'Souvenirs-Regalos',
 			query: '(nwr["shop"="gift"]({{bbox}});node(w););out meta;',
 			iconSrc: imgSrc + 'icones/maxspeed_empty.svg',
 			iconStyle: 'background-color:rgba(255,255,255,0.4)',
