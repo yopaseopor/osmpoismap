6e392caba234693c3bfee79b1c0f2d6fd626a582
yopaseopor <yopaseopor@gmail.com>
2023-07-11 14:51:49 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -20417,6 +20417,42 @@ style: function (feature) {
 				});
 				return style;
 			}
+				},
+		{
+			group: 'Comunitat Catalana',
+			title: 'Refugi climàtic',
+			query: '(nwr["shelter_type"="sun_shelter"]({{bbox}});node(w);nwr["amenity"="harbourmaster"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/maxspeed_empty.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^addr:street$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'icones/maxspeed_empty.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
 },
 		{
 			group: 'Comunidad Española',
