daf226d7bbea9cc72078da8eaa6ba3e045163b1c
yopaseopor <yopaseopor@gmail.com>
2023-08-17 23:26:53 +0200

    Cuina catalana

--- src/config.js
+++ src/config.js
@@ -20453,6 +20453,42 @@ style: function (feature) {
 				});
 				return style;
 			}
+				},
+		{
+			group: 'Comunitat Catalana',
+			title: 'Cuina catalana',
+			query: '(nwr["cuisine"="catalan"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/maxspeed_empty.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'icones/maxspeed_empty.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
 },
 		{
 			group: 'Comunidad Española',
