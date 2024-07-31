a2fb5428d4f1039c92f9533a72ee387b73309b72
yopaseopor <yopaseopor@gmail.com>
2024-07-31 15:35:31 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -3180,6 +3180,42 @@ style: function (feature) {
 			}
 				},
 		{
+			group: 'Ocio',
+			title: 'Tren en miniatura',
+			query: '(nwr["railway"="miniature"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/maxspeed_empty.svg',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(255,0,0,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(255,0,0,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'icones/maxspeed_empty.svg',
+							scale:0.03
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -12,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+				},
+		{
 			group: 'Servicios-Asociaciones',
 			title: 'Caridad',
 			query: '(nwr["shop"="charity"]({{bbox}});node(w););out meta;',
